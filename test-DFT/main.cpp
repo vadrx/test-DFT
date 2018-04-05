767450d9eae382e0aaf162a70772ac1169a6feda
Anton Volkov <vadrx@yandex.ru>
2018-04-05 20:18:21 +0300

    Initial Commit

---
+++ test-DFT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test-DFT
+//
+//  Created by Anton Volkov on 05/04/2018.
+//  Copyright © 2018 Anton Volkov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
